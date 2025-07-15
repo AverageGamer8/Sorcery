b3e1f58996b6d011cc148f3ea579486011797714
Derek Lin <derekhongdalin@gmail.com>
2025-07-15 17:38:39 -0400

    argument parse in main

--- main.cc
+++ main.cc
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
-#include "game.h"
-#include "player.h"
+// #include "game.h"
+// #include "player.h"
 
 int main(int argc, char** argv) {
     std::cout << "start!" << std::endl;
@@ -10,6 +11,10 @@ int main(int argc, char** argv) {
     std::string deck1File;
     std::string deck2File;
     std::string initFile;
+    bool enableGraphics;
+    bool enableTesting;
+
+    // Argument parsing ===================================
 
     for (int i = 1; i < argc; i++) {
         std::string curArg = argv[i];
@@ -22,6 +27,7 @@ int main(int argc, char** argv) {
                 return 1;
             }
             deck1File = argv[i];
+            std::cout << "DEBUG: received - " << deck1File << std::endl;
         } else if (curArg == "-deck2") {
             ++i;
             if (i >= argc) {
@@ -29,6 +35,7 @@ int main(int argc, char** argv) {
                 return 1;
             }
             deck2File = argv[i];
+            std::cout << "DEBUG: received - " << deck2File << std::endl;
         } else if (curArg == "-init") {
             ++i;
             if (i >= argc) {
@@ -36,13 +43,20 @@ int main(int argc, char** argv) {
                 return 1;
             }
             initFile = argv[i];
+            std::cout << "DEBUG: received - " << initFile << std::endl;
         } else if (curArg == "-testing") {
-            
+            enableTesting = true;
+            std::cout << "DEBUG: received - " << enableTesting << std::endl;
         } else if (curArg == "-graphics") {
-            
+            enableGraphics = true;
+            std::cout << "DEBUG: received - " << enableGraphics << std::endl;
         } else {
             std::cerr << "Invalid argument " << curArg << "." << std::endl;
             return 1;
         }
     }
+
+    // Read Files =========================================
+
+
 }

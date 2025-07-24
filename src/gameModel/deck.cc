7744b6696fdd1c11469fc79978d918cdc27f6936
Ken <k67guo@uwaterloo.ca>
2025-07-24 17:03:02 -0400

    flipped deck creation

--- src/gameModel/deck.cc
+++ src/gameModel/deck.cc
@@ -16,91 +16,91 @@ void Deck::loadDeck(ifstream& inf, Game* game) {
     while (getline(inf, name)) {
         if (name == "Air Elemental") {
             auto card = make_shared<AirElemental>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Earth Elemental") {
             auto card = make_shared<EarthElemental>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Bone Golem") {
             auto card = make_shared<BoneGolem>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Fire Elemental") {
             auto card = make_shared<FireElemental>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Potion Seller") {
             auto card = make_shared<PotionSeller>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Novice Pyromancer") {
             auto card = make_shared<NovicePyromancer>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Apprentice Summoner") {
             auto card = make_shared<ApprenticeSummoner>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Master Summoner") {
             auto card = make_shared<MasterSummoner>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Banish") {
             auto card = make_shared<Banish>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Unsummon") {
             auto card = make_shared<Unsummon>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Recharge") {
             auto card = make_shared<Recharge>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Disenchant") {
             auto card = make_shared<Disenchant>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Raise Dead") {
             auto card = make_shared<RaiseDead>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Blizzard") {
             auto card = make_shared<Blizzard>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Giant Strength") {
             auto card = make_shared<GiantStrength>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Enrage") {
             auto card = make_shared<Enrage>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Haste") {
             auto card = make_shared<Haste>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Magic Fatigue") {
             auto card = make_shared<MagicFatigue>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Silence") {
             auto card = make_shared<Silence>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Dark Ritual") {
             auto card = make_shared<DarkRitual>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Aura of Power") {
             auto card = make_shared<AuraOfPower>(owner, game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
         else if (name == "Standstill") {
             auto card = make_shared<Standstill>(owner,game);
-            cards.emplace_back(card);
+            cards.emplace(cards.begin(), card);
         }
     }
 }
